7e347c5dae6e076d1affb8b44baa5a6f1c067386
mdabdullahm <you@example.com>
2025-02-15 12:45:25 +0600

    explore event state, use effact

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
+  const [bookmarks, setBookmarks] = useState([]);
+  const [readingTime, setRedingTime] = useState(0)
+
+  const handleAddToBookmark = blog =>{
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
+  }
+
+  const handleMarkAsRead = (id, time) =>{
+    const newRedingTime = readingTime + time;
+    setRedingTime(newRedingTime);
+    // console.log('remove bookmark', id)
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
+    setBookmarks(remainingBookmarks);
+  }
 
   return (
     <>
       <Header></Header>
-      <div className='md:flex'>
-      <Blogs></Blogs>
-      <Bookmarks></Bookmarks>
+      <div className='md:flex max-w-7xl mx-auto'>
+      <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+      <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
       </div>
       
     </>
